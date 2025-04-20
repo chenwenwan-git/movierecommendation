cf28a4f6822daa93c7b13e227d7b0c485ba56780
Chen <Chen@123.com>
2025-04-20 18:13:15 +0800

    首页轮播图电影名称

--- src/views/movie/FirstPage.vue
+++ src/views/movie/FirstPage.vue
@@ -18,17 +18,17 @@ onMounted(() => {
 
 <template>
     
-  <el-carousel :interval="4000" type="card" height="600px" style="width: 700px; margin: 0 auto;">
+  <el-carousel :interval="4000" type="card" height="600px" style="width: 700px; margin: 0 auto;" @change="handleCarouselChange">
     <el-carousel-item v-for="item in MovieHotList.slice(0,3)" :key="item.movie_id">
         
-        <img :src="item.postUrl" alt="轮播图片" style="width: 100%; height: 100%; object-fit: cover;">
+        <img src="@/assets/1.png" alt="轮播图片" style="width: 100%; height: 100%; object-fit: cover;">
        <!-- 图片连接用不了吗 -->
       
-      
+      <div class="moviename">
+   {{ item.movieName }}</div>
     </el-carousel-item>
   </el-carousel>
-    <div class="moviename">
-    <el-button v-for="item in MovieHotList.slice(0,3)" :key="item.movie_id" :class="{active:item.id === '2'}">{{ item.movieName }}</el-button></div>
+    
     
     <FirstPageBox :movieList="MovieHotList">评分最多……</FirstPageBox>
 </template>
@@ -51,23 +51,17 @@ onMounted(() => {
   background-color: #d3dce6;
 }
 .moviename{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 20px;
-    flex-wrap: wrap;
+    position: absolute;
+    bottom:0;
+    width: 100%;
+    background-color: rgba(203, 203, 203, 0.598);
+    text-align: center;
+    color:white;
+    height: 50px;
+    line-height: 50px;
 }
-.el-button{
-    margin: 10px;
-    background-color: #bcbebf;
-    color: white;
-    border-radius: 5px;
-    padding: 10px 20px;
-    font-size: 16px;
-    cursor: pointer;
-   
-    
-}
+
+
  .active{
         background-color: red;
         
